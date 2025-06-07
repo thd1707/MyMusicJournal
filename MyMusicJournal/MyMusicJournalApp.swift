76093678c49b232f31bc4a51f13cb064e658a7c7
Hong Duc <tranhongduchkn@gmail.com>
2025-06-07 11:55:35 +0700

    Initial Commit

---
+++ MyMusicJournal/MyMusicJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicJournalApp.swift
+//  MyMusicJournal
+//
+//  Created by TY on 07/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
